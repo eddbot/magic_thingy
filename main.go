8de00e077bc862bfeedce60e1b6c93cc859fd9fb
eddbot <edwardsansome@gmail.com>
2023-06-17 22:43:38 +0100

    rough debouncer

--- main.go
+++ main.go
@@ -1,5 +1,22 @@
 package main
 
+import (
+	"log"
+	"net/http"
+)
+
 func main() {
-	cli()
+
+	trie := CreateNameTrie()
+
+	fs := http.FileServer(http.Dir("frontend"))
+
+	http.Handle("/", fs)
+
+	http.HandleFunc("/api/card", func(w http.ResponseWriter, r *http.Request) {
+		name := r.URL.Query().Get("name")
+		log.Println(trie.getFuzzy(name))
+	})
+	log.Println("Starting server on port 3000")
+	log.Fatal(http.ListenAndServe(":3000", nil))
 }
